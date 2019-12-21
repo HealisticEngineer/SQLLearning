d4380afb29dad555557373bca8d59219ff0c386e
HealisticEngineer <johnedwardhall@gmail.com>
2019-12-21 09:50:43 +0100

    Example for Key rotation
    Commands needed to create a new key and rotate the key on example database.

---
+++ AlwaysEncrypted/RotateAlwaysEncryptedCert.ps1
@@ -0,0 +1,59 @@
+# Create a new column master key in Windows Certificate Store.
+$cert = New-SelfSignedCertificate -Subject "AlwaysEncryptedCert 2" `
+-CertStoreLocation Cert:CurrentUser\My -KeyExportPolicy Exportable `
+-Type DocumentEncryptionCert -KeyUsage KeyEncipherment -KeySpec KeyExchange `
+-KeyLength 2048
+
+# Import the SqlServer module
+Import-Module "SqlServer"
+
+# Connect to your database.
+$serverName = "$env:COMPUTERNAME"
+$databaseName = "Example"
+# Change the authentication method in the connection string, if needed.
+$connStr = "Server = " + $serverName + "; Database = " + $databaseName + "; Integrated Security = True"
+$database = Get-SqlDatabase -ConnectionString $connStr
+
+# Create a SqlColumnMasterKeySettings object for your new column master key. 
+$newCmkSettings = New-SqlCertificateStoreColumnMasterKeySettings -CertificateStoreLocation "CurrentUser" -Thumbprint $cert.Thumbprint
+
+# Create metadata for your new column master key in the database.
+$newCmkName = "CMK2"
+New-SqlColumnMasterKey -Name $newCmkName -InputObject $database -ColumnMasterKeySettings $newCmkSettings
+
+# Initiate the rotation from the current column master key to the new column master key.
+$oldCmkName = "CMK1"
+Invoke-SqlColumnMasterKeyRotation -SourceColumnMasterKeyName $oldCmkName -TargetColumnMasterKeyName $newCmkName -InputObject $database
+
+# Complete the rotation of the old column master key.
+Complete-SqlColumnMasterKeyRotation -SourceColumnMasterKeyName $oldCmkName  -InputObject $database
+
+# Remove the old column master key metadata.
+Remove-SqlColumnMasterKey -Name $oldCmkName -InputObject $database
+
+
+# Generate a new column encryption key, encrypt it with the column master key and create column encryption key metadata in the database. 
+$cmkName = "CMK2"
+$newCekName = "CEK2"
+New-SqlColumnEncryptionKey -Name $newCekName -InputObject $database -ColumnMasterKey $cmkName 
+
+
+# Find all columns encrypted with the old column encryption key, and create a SqlColumnEncryptionSetting object for each column.
+$ces = @()
+$oldCekName = "CEK1"
+$tables = $database.Tables
+for($i=0; $i -lt $tables.Count; $i++){
+    $columns = $tables[$i].Columns
+    for($j=0; $j -lt $columns.Count; $j++) {
+        if($columns[$j].isEncrypted -and $columns[$j].ColumnEncryptionKeyName -eq $oldCekName) {
+            $threeColPartName = $tables[$i].Schema + "." + $tables[$i].Name + "." + $columns[$j].Name 
+            $ces += New-SqlColumnEncryptionSettings -ColumnName $threeColPartName -EncryptionType $columns[$j].EncryptionType -EncryptionKey $newCekName
+        }
+     }
+}
+
+# Re-encrypt all columns, currently encrypted with the old column encryption key, using the new column encryption key.
+Set-SqlColumnEncryption -ColumnEncryptionSettings $ces -InputObject $database -UseOnlineApproach -MaxDowntimeInSeconds 120 -LogFileDirectory .
+
+# Remove the old column encryption key metadata.
+Remove-SqlColumnEncryptionKey -Name $oldCekName -InputObject $database
